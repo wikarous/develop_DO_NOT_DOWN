39c43a55b708c2de67b5981a79f5296f9220ad7e
MunSeongSik <69944438+MunSeongSik@users.noreply.github.com>
2020-10-05 20:46:18 +0900

    Update interface.py

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -90,7 +90,7 @@ class CarInterface(CarInterfaceBase):
       ret.lateralTuning.lqr.b = [-1.92006585e-04, 3.95603032e-05]
       ret.lateralTuning.lqr.c = [1., 0.]
       ret.lateralTuning.lqr.k = [-100., 450.]
-      ret.lateralTuning.lqr.l = [0.22, 0.318]
+      ret.lateralTuning.lqr.l = [0.220, 0.318]
       ret.lateralTuning.lqr.dcGain = 0.00274
       #lqr 조향 종료 
    
