5c9ad562e378d9edb6c8091f3ae9d801427bebe1
stonerains <72440538+stonerains@users.noreply.github.com>
2020-10-11 20:55:30 +0900

    Update nanovg.c
    원복

--- phonelibs/nanovg/nanovg.c
+++ phonelibs/nanovg/nanovg.c
@@ -2292,7 +2292,7 @@ int nvgAddFallbackFont(NVGcontext* ctx, const char* baseFont, const char* fallba
 void nvgFontSize(NVGcontext* ctx, float size)
 {
 	NVGstate* state = nvg__getState(ctx);
-	state->fontSize = size * 0.9f;
+	state->fontSize = size;
 }
 
 void nvgFontBlur(NVGcontext* ctx, float blur)
